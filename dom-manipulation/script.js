ae16db784464a1bae422bfcaa49a0ad08ebb3192
Inene Israel Oluwaniyi <Israelinene91@gmail.com>
2025-02-08 22:24:15 +0100

    task 1 upload

---
+++ dom-manipulation/script.js
@@ -0,0 +1,31 @@
+const quotes = [
+  { text: "Knowledge is Power", category: "knowledge" },
+  { text: "health is wealth", category: "health" },
+];
+
+function getRandomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function addQuote() {
+  let newQuoteText = document.getElementById("newQuoteText");
+  let newQuoteCategory = document.getElementById("newQuoteCategory");
+
+  const quote = { text: newQuoteText.value, category: newQuoteCategory.value };
+  quotes.push(quote);
+
+  newQuoteCategory.value = "";
+  newQuoteText.value = "";
+}
+
+function displayRandomQuote() {
+  const quoteDisplay = document.getElementById("quoteDisplay");
+
+  quoteDisplay.style.display = "block";
+  quoteDisplay.textContent = getRandomItem(quotes).text;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  displayRandomQuote();
+  newQuote.addEventListener("click", displayRandomQuote);
+});
